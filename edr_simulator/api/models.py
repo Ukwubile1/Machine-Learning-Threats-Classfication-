a4c11ed74f05538bf4db936141ffc4adaa721fb6
Ukwubile1 <Ukwubilejoshuae@gmail.com>
2025-03-23 14:53:13 +0100

    Update models.py

--- edr_simulator/api/models.py
+++ edr_simulator/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class ThreatDetectionLog(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True)
+    features = models.JSONField()
+    prediction = models.BooleanField()
+    probability = models.FloatField()
+
+    def __str__(self):
+        return f"Detection at {self.timestamp} - Threat: {self.prediction}"
